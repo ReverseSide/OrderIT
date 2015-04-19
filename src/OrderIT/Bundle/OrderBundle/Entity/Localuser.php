1df0847f8b093879507a1b59d4f6ce70397cfae5
raccoonhoax{mac} <vincentmontet.vm@gmail.com>
2015-04-19 17:01:33 +0200

    Ldap marche

--- src/OrderIT/Bundle/OrderBundle/Entity/Localuser.php
+++ src/OrderIT/Bundle/OrderBundle/Entity/Localuser.php
@@ -2,154 +2,52 @@
 
 namespace OrderIT\Bundle\OrderBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
+use FR3D\LdapBundle\Model\LdapUserInterface as LdapUserInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Localuser
- *
- * @ORM\Table(name="localuser")
  * @ORM\Entity
+ * @ORM\Table(name="localuser")
  */
-class Localuser
+class Localuser extends BaseUser implements LdapUserInterface
 {
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id_user", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $idUser;
-
+    protected $id;
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="group_id_group", type="integer", nullable=true)
+     * @ORM\Column(type="string")
      */
-    private $groupIdGroup;
-
+    protected $dn;
     /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=45, nullable=true)
+     * @ORM\Column(type="string")
      */
-    private $name;
-
+    protected $name;
     /**
-     * @var string
-     *
-     * @ORM\Column(name="vorname", type="string", length=45, nullable=true)
+     * @ORM\Column(type="string")
      */
-    private $vorname;
+    protected $language;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="mail", type="string", length=45, nullable=true)
-     */
-    private $mail;
-
-
-
-    /**
-     * Get idUser
-     *
-     * @return integer 
-     */
-    public function getIdUser()
+    public function __construct()
     {
-        return $this->idUser;
+        parent::__construct();
+        if (empty($this->roles)) {
+            $this->roles[] = 'ROLE_USER';
+        }
     }
-
-    /**
-     * Set groupIdGroup
-     *
-     * @param integer $groupIdGroup
-     * @return Localuser
-     */
-    public function setGroupIdGroup($groupIdGroup)
-    {
-        $this->groupIdGroup = $groupIdGroup;
-
-        return $this;
+    public function setDn($dn) {
+        $this->dn = $dn;
     }
-
-    /**
-     * Get groupIdGroup
-     *
-     * @return integer 
-     */
-    public function getGroupIdGroup()
-    {
-        return $this->groupIdGroup;
+    public function getDn() {
+        return $this->dn;
     }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Localuser
-     */
-    public function setName($name)
-    {
+    public function setName($name) {
         $this->name = $name;
-
-        return $this;
     }
-
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set vorname
-     *
-     * @param string $vorname
-     * @return Localuser
-     */
-    public function setVorname($vorname)
-    {
-        $this->vorname = $vorname;
-
-        return $this;
-    }
-
-    /**
-     * Get vorname
-     *
-     * @return string 
-     */
-    public function getVorname()
-    {
-        return $this->vorname;
-    }
-
-    /**
-     * Set mail
-     *
-     * @param string $mail
-     * @return Localuser
-     */
-    public function setMail($mail)
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    /**
-     * Get mail
-     *
-     * @return string 
-     */
-    public function getMail()
-    {
-        return $this->mail;
+    public function setLanguage($language) {
+        $this->language = $language;
     }
 }
